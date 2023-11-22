9cbf697570e8a43076ff218dc61e929ec6468bf7
Aliona Matweyenko <matveenkoalena2844@mail.ru>
2023-11-23 01:30:02 +0300

    Create context processors for showing user image

--- onlineStore/context_processors.py
+++ onlineStore/context_processors.py
@@ -1,5 +1,6 @@
 from cart.utils.cart import Cart
 from shop.models import Category
+from accounts.models import User
 
 
 def return_cart(request):
@@ -10,3 +11,10 @@ def return_cart(request):
 def return_categories(request):
     categories = Category.objects.all()
     return {'categories': categories}
+
+
+def user_image(request):
+    if request.user.is_authenticated:
+        user = User.objects.get(id=request.user.id)
+        return {'user_image': user.image.url}
+    return {}
